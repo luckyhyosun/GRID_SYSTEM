b883dfb70fac342d9443990792756679faf2c546
HellobonjourLucky <73243458+HellobonjourLucky@users.noreply.github.com>
2021-10-30 13:26:16 +0200

    2nd commit

--- app.js
+++ app.js
@@ -5,18 +5,32 @@ const icon2 = document.getElementById("b");
 const icon3 = document.getElementById("c");
 const nav = document.getElementById('nav');
 const navList = document.querySelector('.navList');
+const navRangeBar = document.querySelector('.navRangeBar');
 let navListPics;
 let navSelectedList = [];
 let smallRectHtml;
+let navRangeValue;
+let usingGridArray = [];
 
 const width_array = [0, 100, 200, 300];
 const height_array = [0, 100, 200, 300, 400, 500];
 const rotate_deg = [0, 90, 180, 270];
 
+//initial setting
+// localStorage.setItem('grids', 3);
+// localStorage.setItem('blocks', 7);
 
-function howManyGrid(num){
-  console.log(num);
+//refresh with nav setting
+document.querySelector('.btn').addEventListener('click', function(){
+  location.reload();
+})
+if(localStorage.getItem('navState') == 'on'){
+  nav.classList.add('show');
+}else{
+  nav.classList.remove('show');
 }
+const blocksNum = localStorage.getItem('blocks');
+
 
 //nav
 icon.addEventListener('click', function() {
@@ -24,7 +38,14 @@ icon.addEventListener('click', function() {
   icon2.classList.toggle('c');
   icon3.classList.toggle('b');
   nav.classList.toggle('show');
+  if(nav.classList.contains('show')){
+    localStorage.setItem('navState', 'on');
+  }else{
+    localStorage.setItem('navState', 'off');
+  }
 });
+
+
 
 //making nav list
 for(i = 0; i < 4; i++){
@@ -35,20 +56,53 @@ for(i = 0; i < 4; i++){
   `
 }
 
-//setting small grids for shuffle
-//for loop 다음에 html tag가 만들어진 뒤, getElement해야 한다.
+//main functions
+function howManyGrid(array, num){
+  // console.log(array, num);
+  localStorage.setItem('grids', num);
+}
+
+function howManyBlocks(num){
+  // console.log(num);
+  localStorage.setItem('blocks', num);
+}
+
+
+
+//CHOOSE HOW MANY GRIDS ARE GONNA USED..
 navListPics = Array.from(document.getElementsByClassName('navListPic'));
+//각각의 그리드 이미지를가 html tag가 만들어진 뒤, getElement해야 한다.
+
 navListPics.forEach(data => {
   data.addEventListener('click', function(){
     data.classList.toggle('listSelected');
-    navListPicValue = data.getAttribute("value");
 
     //toggle 되어 select된 그리드만 모은 Array.
+    const valueArray = [];
     const selectedListPics = document.getElementsByClassName('listSelected');
-    navSelectedList = Array.from(selectedListPics)
-    console.log(navSelectedList);
+    navSelectedList = Array.from(selectedListPics);
+    navSelectedList.forEach(data => {
+      valueArray.push(data.getAttribute('value'));
+      // console.log(valueArray);
+    })
+    howManyGrid(navSelectedList, valueArray);
   })
 })
+
+
+
+//CHOOSE HOW MANY BLOCKS ARE GONNA USED..
+var target = document.querySelector('.navRangeTextValue');
+if(blocksNum){
+  target.innerHTML = blocksNum;
+  navRangeBar.value = blocksNum;
+}
+navRangeBar.addEventListener("input", function(){
+  navRangeValue = navRangeBar.value;
+  target.innerHTML = navRangeValue;
+  // console.log(navRangeValue);
+  howManyBlocks(navRangeValue);
+});
 
 
 
